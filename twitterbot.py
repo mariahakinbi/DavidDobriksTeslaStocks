bf59745d245bb6d08019740abb50f8457c6408b1
Mariah Akinbi <mariah.akinbi@gmail.com>
2017-11-12 22:42:01 -0600

    working around encoding errors

--- twitterbot.py
+++ twitterbot.py
@@ -12,16 +12,40 @@ api = tweepy.API(auth)
 # api.update_status(status=tweet)
 
 # Open text file verne.txt (or your chosen file) for reading
-my_file = open('textfile.txt', 'r')
+my_file = open('textfile.txt', 'r', encoding='utf-8')
+# print(st.encode('ascii').decode('unicode-escape').encode('iso-8859-1').decode('utf-8'))
 
 # Read lines one by one from my_file and assign to file_lines variable
-file_lines = my_file.readlines()
+file_lines = my_file.read()
 
 # Close file
 my_file.close()
 
 from time import sleep
 
+skip_these_char = ['\u2019','\u201c','\u201d','\u2013']
+
+sentences =''
+
 for line in file_lines:
-    # line = unicode(line, 'utf-8')
-    print(line)
+    if line in skip_these_char:
+        continue
+    sentences+=line
+
+
+
+#WHEN U SEE A NEWLINE CHAR: ADD THE STUFF BEFORE IT TO A LSIT AS A STRING
+tweets = []
+line =''
+for char in sentences:
+    if char =='\n':
+        tweets+=line
+    else:
+        line+=char
+print(len(tweets))
+print(tweets)
+# print(sentences)
+# for line in sentences:
+#     print (line)
+
+# print(sentences.split("\n")[0])
